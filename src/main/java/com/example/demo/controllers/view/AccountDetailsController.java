7f5b7fa36de405e1176716e60726dca543b36ecc
Adrian Rubak <adek.rubak@gmail.com>
2021-12-20 22:37:47 +0100

    displaying list of transfers

--- src/main/java/com/example/demo/controllers/view/AccountDetailsController.java
+++ src/main/java/com/example/demo/controllers/view/AccountDetailsController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers.view;
 import com.example.demo.entities.bankAccount.BankAccount;
 import com.example.demo.entities.bankAccount.BankAccountRepository;
 import com.example.demo.entities.user.User;
+import com.example.demo.transfers.TransferService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,11 @@ import javax.servlet.http.HttpSession;
 public class AccountDetailsController {
     private String prefix = "/client/";
     private BankAccountRepository bankAccountRepository;
+    private TransferService transferService;
 
-    public AccountDetailsController(BankAccountRepository bankAccountRepository) {
+    public AccountDetailsController(BankAccountRepository bankAccountRepository, TransferService transferService) {
         this.bankAccountRepository = bankAccountRepository;
+        this.transferService = transferService;
     }
 
     @GetMapping("/account")
@@ -26,7 +29,7 @@ public class AccountDetailsController {
         User user = (User) httpSession.getAttribute("user");
         BankAccount bankAccount = bankAccountRepository.getBankAccount(accountNumber);
         model.addAttribute("account", bankAccount);
-
+        model.addAttribute("assignedTransfers", transferService.getAssignedTransfers(bankAccount));
         return prefix + "account";
     }
 
--- src/main/java/com/example/demo/entities/transfers/Transfer.java
+++ src/main/java/com/example/demo/entities/transfers/Transfer.java
@@ -1,33 +1,28 @@
 package com.example.demo.entities.transfers;
 
-import java.math.BigDecimal;
+import lombok.Data;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+@Data
 public class Transfer {
     private final String id;
     private final String senderId;
     private final String receiverId;
     private final BigDecimal amount;
+    private Date transferDate;
 
     public Transfer(String id, String senderId, String receiverId, BigDecimal amount) {
         this.id = id;
         this.senderId = senderId;
         this.receiverId = receiverId;
         this.amount = amount;
-    }
 
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getSenderId() {
-        return senderId;
-    }
-
-    public String getReceiverId() {
-        return receiverId;
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+        this.transferDate = Date.valueOf(dateTimeFormatter.format(LocalDateTime.now()));
     }
 }
